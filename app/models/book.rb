6ccc89fb8641445e83497b30ea7f0fb6a5ce8b48
Ishwor-shrestha1205 <shrestha_ishwor@retail-ai.jp>
2021-12-28 22:23:12 +0900

    ActiveRecord::Enumの導入

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
+    enum sales_status: {
+        reservation: 0,
+        now_on_sale: 1,
+        end_of_print: 2,
+    }
     scope :costly, -> {where("price > ?", 3000)}
     # Book.costly.written_about("java")書くことで、3000円以上のjava書いている本を探す。
     scope :written_about, -> (theme) { where("name > ?", "%#{theme}%")}
@@ -7,4 +12,33 @@ class Book < ApplicationRecord
     belongs_to :publisher
     has_many :book_authors
     has_many :authors, through: :book_authors
+    validates :name, presence: true
+    validates :name, length: { maximum: 25}
+    validates :price, numericality: {greater_than_or_equal_to: 0}
+    validate do |book|
+        if book.name.include?("exercise")
+            book.errors[:name] << "I don't like exercise."
+        end
+    end
+
+    before_validation :add_lovely_to_cat
+
+    def add_lovely_to_cat
+        self.name = self.name.gsub(/Cat/) do |matched|
+            "lovely #{matched}"
+        end
+    end
+
+    after_destroy do
+        Rails.logger.info "Book is deleted: #{self.attributes}"
+    end
+
+    after_destroy :if => :high_price? do
+        Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+        Rails.logger.warn "Please check!!"
+    end
+
+    def high_price?
+        price >= 5000
+    end
 end
